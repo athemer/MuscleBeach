e1fdda8349ece5aeaf064fb11eba023d0076d827
athemer <athemer0520@gmail.com>
2017-03-23 11:58:29 +0800

    calendar implemented!!

---
+++ MuscleBeach/CalendarViewController.swift
@@ -0,0 +1,116 @@
+//
+//  CalendarViewController.swift
+//  MuscleBeach
+//
+//  Created by 陳冠華 on 2017/3/23.
+//  Copyright © 2017年 my app. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CalendarViewController: UIViewController, JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate {
+
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
+    
+    
+    let white = UIColor(colorWithHexValue: 0xECEAED)
+    let darkPurple = UIColor(colorWithHexValue: 0x3A284C)
+    let dimPurple = UIColor(colorWithHexValue: 0x574865)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        calendarView.dataSource = self
+        calendarView.delegate = self
+        calendarView.registerCellViewXib(file: "CellView") // Registering your cell is manditory
+        calendarView.cellInset = CGPoint(x: 0, y: 0)       // default is (3,3)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        
+        let startDate = formatter.date(from: "2016 02 01")! // You can use date generated from a formatter
+        let endDate = Date()                                // You can also use dates created from this function
+        let parameters = ConfigurationParameters(startDate: startDate,
+                                                 endDate: endDate,
+                                                 numberOfRows: 6, // Only 1, 2, 3, & 6 are allowed
+            calendar: Calendar.current,
+            generateInDates: .forAllMonths,
+            generateOutDates: .tillEndOfGrid,
+            firstDayOfWeek: .sunday)
+        return parameters
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, willDisplayCell cell: JTAppleDayCellView, date: Date, cellState: CellState) {
+
+        guard let myCustomCell = cell as? CellView else { return }
+
+        // Setup Cell text
+        myCustomCell.dayLabel.text = cellState.text
+        
+        handleCellTextColor(view: cell, cellState: cellState)
+        handleCellSelection(view: cell, cellState: cellState)
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleDayCellView?, cellState: CellState) {
+        
+        handleCellSelection(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleDayCellView?, cellState: CellState) {
+        
+        handleCellSelection(view: cell, cellState: cellState)
+        handleCellTextColor(view: cell, cellState: cellState)
+    }
+    
+    // Function to handle the text color of the calendar
+    func handleCellTextColor(view: JTAppleDayCellView?, cellState: CellState) {
+        
+        guard let myCustomCell = view as? CellView  else {
+            return
+        }
+        
+        if cellState.isSelected {
+            myCustomCell.dayLabel.textColor = darkPurple
+        } else {
+            if cellState.dateBelongsTo == .thisMonth {
+                myCustomCell.dayLabel.textColor = white
+            } else {
+                myCustomCell.dayLabel.textColor = dimPurple
+            }
+        }
+    }
+    
+    // Function to handle the calendar selection
+    func handleCellSelection(view: JTAppleDayCellView?, cellState: CellState) {
+        guard let myCustomCell = view as? CellView  else {
+            return
+        }
+        if cellState.isSelected {
+            myCustomCell.selectedView.layer.cornerRadius = 20
+            myCustomCell.selectedView.isHidden = false
+        } else {
+            myCustomCell.selectedView.isHidden = true
+        }
+    }
+}
+
+extension UIColor {
+    convenience init(colorWithHexValue value: Int, alpha:CGFloat = 1.0){
+        self.init(
+            red: CGFloat((value & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((value & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(value & 0x0000FF) / 255.0,
+            alpha: alpha
+        )
+    }
+}
---
+++ MuscleBeach/CellView.swift
@@ -0,0 +1,17 @@
+//
+//  CellView.swift
+//  MuscleBeach
+//
+//  Created by 陳冠華 on 2017/3/23.
+//  Copyright © 2017年 my app. All rights reserved.
+//
+
+import Foundation
+import JTAppleCalendar
+
+class CellView: JTAppleDayCellView {
+    
+    @IBOutlet weak var dayLabel: UILabel!
+    
+    @IBOutlet weak var selectedView: UIView!
+}
