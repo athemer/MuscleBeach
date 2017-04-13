bd165abea400fd35440a4482c22d9183a8366835
athemer <athemer0520@gmail.com>
2017-04-13 18:08:35 +0800

    before revision of collection view cell

---
+++ MuscleBeach/FirstCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FirstCollectionViewCell.swift
+//  MuscleBeach
+//
+//  Created by 陳冠華 on 2017/4/13.
+//  Copyright © 2017年 my app. All rights reserved.
+//
+
+import UIKit
+
+class FirstCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ MuscleBeach/SecondCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SecondCollectionViewCell.swift
+//  MuscleBeach
+//
+//  Created by 陳冠華 on 2017/4/13.
+//  Copyright © 2017年 my app. All rights reserved.
+//
+
+import UIKit
+
+class SecondCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
